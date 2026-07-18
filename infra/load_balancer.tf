b9c5f9de8386daf03ea9520633063a834f84cdf9
Sinjini-D3V <sinjini@d3vtech.com>
2026-07-18 17:16:37 +0000

    feat: per-LB managed cert domains + HTTP->HTTPS redirect

--- infra/load_balancer.tf
+++ infra/load_balancer.tf
@@ -56,7 +56,8 @@ locals {
     provision_cloud_run              = var.provision_cloud_run
     standalone_vm_neg_name           = format("projects/%s/zones/%s/instanceGroups/%s", var.project_id, local.standalone_vm_zone, var.standard_vm_name)
     spot_vm_neg_name                 = format("projects/%s/zones/%s/instanceGroups/%s", var.project_id, local.spot_vm_zone, var.spot_vm_name)
-    ssl_domains                      = var.domains
+    spot_domain                      = try(var.domains[1], "beta.erp.sapphirefountains.com")
+    production_domain                = try(var.domains[0], "erp.sapphirefountains.com")
   })
 
   load_balancer_config = try(yamldecode(local._lb_template), tomap({}))
@@ -128,4 +129,48 @@ module "load_balancer" {
     module.spot_vm,
     module.compute_vm
   ]
+}
+
+# HTTP-to-HTTPS redirect resources
+locals {
+  http_redirect_configs = var.provision_ips && length(module.ips) > 0 ? {
+    for k, v in local.load_balancer_config : k => {
+      ip_address = k == var.spot_lb_name
+        ? module.ips[0].global_addresses[var.spot_glb_ip_name].address
+        : module.ips[0].global_addresses[var.glb_ip_name].address
+    }
+  } : {}
+}
+
+resource "google_compute_url_map" "http_redirect" {
+  for_each    = local.http_redirect_configs
+  provider    = google-beta
+  project     = var.project_id
+  name        = "${each.key}-http-redirect"
+  description = "HTTP to HTTPS redirect for ${each.key}"
+
+  default_url_redirect {
+    https_redirect         = true
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+    strip_query            = false
+  }
+}
+
+resource "google_compute_target_http_proxy" "redirect" {
+  for_each = local.http_redirect_configs
+  provider = google-beta
+  project  = var.project_id
+  name     = "${each.key}-http-proxy"
+  url_map  = google_compute_url_map.http_redirect[each.key].id
+}
+
+resource "google_compute_global_forwarding_rule" "http_redirect" {
+  for_each    = local.http_redirect_configs
+  provider    = google-beta
+  project     = var.project_id
+  name        = "${each.key}-http-rule"
+  ip_address  = each.value.ip_address
+  ip_protocol = "TCP"
+  port_range  = "80"
+  target      = google_compute_target_http_proxy.redirect[each.key].id
 }
